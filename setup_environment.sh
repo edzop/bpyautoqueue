a21e354e3082122f9a4295a39c51826b623ef0ac
Your Name <ek>
2022-12-18 21:54:00 +0800

    add bin sh environment qualifier

--- setup_environment.sh
+++ setup_environment.sh
@@ -3,8 +3,10 @@ current_dir=`pwd`
 #ln -s -i $current_dir/blqaf.sh /usr/bin/blqaf
 #ln -s -i $current_dir/blq.sh /usr/bin/blq
 
-echo "python3 $current_dir/render_db.py \"\$@\"" > /usr/bin/blq
+echo "#!/bin/sh" > /usr/bin/blq
+echo "python3 $current_dir/render_db.py \"\$@\"" >> /usr/bin/blq
 chmod +x /usr/bin/blq
 
-echo "python3 $current_dir/bake_db.py \"\$@\"" > /usr/bin/blb
+echo "#!/bin/sh" > /usr/bin/blb
+echo "python3 $current_dir/bake_db.py \"\$@\"" >> /usr/bin/blb
 chmod +x /usr/bin/blb
