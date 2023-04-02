41ad67e03ef84febeb6d18caa0eff5221adc5a1c
Your Name <ek>
2023-04-02 14:34:55 +0800

    move to pymovieutils

--- resolve/setup.sh
+++
@@ -1,9 +0,0 @@
-export RESOLVE_SCRIPT_API="/opt/resolve/Developer/Scripting"
-export RESOLVE_SCRIPT_LIB="/opt/resolve/libs/Fusion/fusionscript.so"
-#export PYTHONPATH="$PYTHONPATH:$RESOLVE_SCRIPT_API/Modules/"
-
-export PYTHONPATH="$PYTHONPATH:/opt/resolve/Developer/Scripting/Modules"
-
-echo $PYTHONPATH
-
-python3 test.py
--- resolve/test_list_media.py
+++
@@ -1,5 +0,0 @@
-import resolve_helper
-
-rh = resolve_helper.resolve_helper()
-
-rh.list_media_in_pool(rh.activeproject)
